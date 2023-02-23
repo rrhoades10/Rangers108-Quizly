6fd44a15b3c5734a8b9b1bcdb1b650e6c67cf39f
rrhoades10 <rrhoades10@yahoo.com>
2023-02-23 14:01:10 -0600

    initial commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,65 @@
+const { UserType, QuizType, SubmissionType } = require('./types')
+const { User, Quiz, Submission } = require('../models')
+const { GraphQLString, GraphQLID, GraphQLList } = require('graphql')
+
+
+// query all users
+const users = {
+    type: new GraphQLList(UserType),
+    description: 'Query a list of users',
+    resolve(parent, args) {
+        return User.find()
+    }
+}
+
+// user by id
+/* 
+* Get user by id
+*/
+const user = {
+    type: UserType,
+    description: 'Query a user by their ID',
+    args: {
+        id: { type: GraphQLID }
+    },
+    resolve(parent, args) {
+        return User.findById(args.id)
+    }
+}
+
+
+
+// query quiz by slug(quiz title)
+
+const quizBySlug = {
+    type: QuizType,
+    description: 'Query a quiz by its slug',
+    args: {
+        slug: { type: GraphQLString }
+    },
+    resolve(parent, args) {
+        return Quiz.findOne({
+            slug: args.slug
+        })
+    }
+}
+
+
+const submission = {
+    type: SubmissionType,
+    description: 'Query a submission by their ID',
+    args: {
+        id: { type: GraphQLID }
+    },
+    resolve(parent, args) {
+        return Submission.findById(args.id)
+    }
+}
+
+
+module.exports = {
+    users,
+    user,
+    quizBySlug,
+    submission
+}
